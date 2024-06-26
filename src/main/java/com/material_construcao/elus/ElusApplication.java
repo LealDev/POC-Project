c33941310d5f8357cddbe27141e618b44d735f1f
LealDev <lealmaster0111@gmail.com>
2024-06-26 18:44:49 -0300

    Initial commit

---
+++ src/main/java/com/material_construcao/elus/ElusApplication.java
@@ -0,0 +1,13 @@
+package com.material_construcao.elus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElusApplication.class, args);
+	}
+
+}
